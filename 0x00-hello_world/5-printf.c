902680bf4b18956fc8acfc40e4655a59a0663399
Hillary Kiprotich <hillarykim001@gmail.com>
2023-02-16 21:48:50 +0300

    prints a text using printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,10 @@
 
 /**
  * main - Entry path
- * Description: prints a word
  * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
