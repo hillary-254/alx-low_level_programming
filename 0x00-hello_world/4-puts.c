c0f1aa4e704454707b6ebe258843d08d06734383
Hillary Kiprotich <hillarykim001@gmail.com>
2023-02-16 21:30:54 +0300

    # 4-push.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry path
+ * Description: prints a word
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
