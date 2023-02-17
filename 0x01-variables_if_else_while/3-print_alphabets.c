0aa69216d9d2a54389c533d743a8ca846439b064
Hillary Kiprotich <hillarykim001@gmail.com>
2023-02-17 18:48:27 +0300

    # 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Prints Alphabet
+ * @void:Empty arguments list
+ * Description: Prints alphabet in lowercase then uppercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha_letter = 'a';
+
+	while (alpha_letter <= 'z')
+	{
+		putchar(alpha_letter);
+		alpha_letter++;
+	}
+	alpha_letter = 'A';
+
+	while (alpha_letter <= 'Z')
+	{
+		putchar(alpha_letter);
+		alpha_letter++;
+	}
+	putchar("\n");
+	return (0);
+}
