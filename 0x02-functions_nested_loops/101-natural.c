8769645dd2dd0d7ad10a02d0c4685eab29257eae
Hillary Kiprotich <hillarykim001@gmail.com>
2023-02-21 23:19:17 +0300

    #_101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Sums numbers which are multiple of 3 and 5
+ * Return: 0
+ */
+int main(void)
+{
+	int sum = 0;
+	int n;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if ((n % 3 == 0) || (n % 5 == 0))
+		{
+			sum += n;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
