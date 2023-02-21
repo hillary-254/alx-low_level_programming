a2f51a2b0109fcb28d2adedfae55691bd9154bd9
Hillary Kiprotich <hillarykim001@gmail.com>
2023-02-21 17:27:15 +0300

    #_0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a word
+ * Return: 0
+ */
+int main(void)
+{
+	puts("_putchar");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+We will be dealing with functions and nested loops
