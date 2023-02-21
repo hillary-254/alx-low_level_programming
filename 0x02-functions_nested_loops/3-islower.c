6c4c95794ce4ab4305b6ad7eaec7d40a300dc11d
Hillary Kiprotich <hillarykim001@gmail.com>
2023-02-21 19:26:45 +0300

    #_3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - checks whether a number is lowercase
+ * Return:0
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
