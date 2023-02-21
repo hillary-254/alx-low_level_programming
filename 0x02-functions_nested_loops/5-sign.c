f1ab9b6c8d51a564f1db07de07a1b6a7242894c6
Hillary Kiprotich <hillarykim001@gmail.com>
2023-02-21 20:00:35 +0300

    #_5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - checks if input is either +ve, -ve or 0 integer
+ * @n: the input checked
+ * Return: 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	return (0);
+}
