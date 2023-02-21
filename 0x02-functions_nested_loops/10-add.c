0b446a3ebda8eb429f9e8cff29cc99a1af77d0fc
Hillary Kiprotich <hillarykim001@gmail.com>
2023-02-21 21:17:08 +0300

    #_10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @a: input 1
+ * @b: input 2
+ * Return: 0
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
