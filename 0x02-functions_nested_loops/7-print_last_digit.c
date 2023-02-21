b859387ec92eba150ffdaca9029a1826a735ab56
Hillary Kiprotich <hillarykim001@gmail.com>
2023-02-21 20:43:22 +0300

    #_7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,10 +10,6 @@ int print_last_digit(int num)
 	int lnum;
 
 	lnum = num % 10;
-	if (lnum >= 0)
-	{
-		_putchar(lnum + '0');
-		return (lnum);
-	}
-	return (0);
+	_putchar(lnum + '0');
+	return (lnum);
 }
