57bac4c0f75457a1eb19efbd1df0537a854daae2
Hillary Kiprotich <hillarykim001@gmail.com>
2023-02-21 19:45:50 +0300

    #_4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,10 +3,11 @@
 /**
  * _isalpha - checks if input is a letter, either lowercase or uppercase
  * @c: input to be checked
+ * Return: 0
  */
 int _isalpha(int c)
 {
-	if ((c >=65 && c <= 90) || (c >= 97 && c <= 122))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
 		return (1);
 	}
