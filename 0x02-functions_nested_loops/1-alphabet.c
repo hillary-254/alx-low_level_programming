143ef0700a3a6b50379ab6f2d7c07f4cf485f4d0
Hillary Kiprotich <hillarykim001@gmail.com>
2023-02-21 18:31:38 +0300

    #_1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * main - Prints alphabet
+ * print_alphabet - The function prints alphabet
  * Return: 0
  */
 void print_alphabet(void)
 {
 	char alpha = 'a';
 
-	while(alpha <= 'z')
+	while (alpha <= 'z')
 	{
 		_putchar(alpha);
 		alpha++;
 	}
 	_putchar('\n');
-	return (0);
 }
