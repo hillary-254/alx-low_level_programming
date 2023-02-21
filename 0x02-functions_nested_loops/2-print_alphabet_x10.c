2a398a9b8b3d5541acf136c3899006fd3505533f
Hillary Kiprotich <hillarykim001@gmail.com>
2023-02-21 18:41:31 +0300

    #_2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ * Return: does not have a return value
+ */
+void print_alphabet_x10(void)
+{
+	char alpha = 'a';
+	int i = 0;
+
+	while(i < 10)
+	{
+		while(alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
