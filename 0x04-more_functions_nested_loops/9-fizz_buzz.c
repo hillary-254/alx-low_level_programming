fa4f9682f4b6d3b9332f063437e4f55b5cf17360
Hillary Kiprotich <hillarykim001@gmail.com>
2023-02-23 19:14:00 +0300

    #_9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints num 1-100
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (((num % 3) && (num % 5)) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((num % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((num % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", num);
+		}
+	}
+	return (0);
+}
