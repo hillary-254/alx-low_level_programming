c72b5bb10a2e4a263d3407fec6d6a7b7edec9136
Hillary Kiprotich <hillarykim001@gmail.com>
2023-02-23 18:26:07 +0300

    #_7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal shape
+ * @n: input
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+	int count, size;
+
+	if (n > 0)
+	{
+		for (count = 0; count < n; count++)
+		{
+			for (size = 0; size < count; size++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
