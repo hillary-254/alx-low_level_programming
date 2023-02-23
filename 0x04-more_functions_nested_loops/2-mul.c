ed2e5ccc2e25a24753dc06e9502701a7e267f8f4
Hillary Kiprotich <hillarykim001@gmail.com>
2023-02-23 16:13:25 +0300

    #_2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 numbers
+ * @a: input 1
+ * @b: input 2
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
