d0c94a248b16825c62b37f97612ea17ccfa62004
Hillary Kiprotich <hillarykim001@gmail.com>
2023-02-27 19:01:04 +0300

    #_5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - prints a string in reverse
+ * @s: pointer variable parameter
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+	int x;
+
+	int size = strlen(s);
+
+	_putchar(*s);
+	_putchar('\n');
+	for (x = size - 1; x >= 0; x--)
+	{
+		_putchar(s[x]);
+	}
+	_putchar('\n');
+}
