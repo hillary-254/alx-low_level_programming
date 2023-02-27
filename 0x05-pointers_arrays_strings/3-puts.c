2919a5f60eae6ebc05280b081e54c70d86b0d026
Hillary Kiprotich <hillarykim001@gmail.com>
2023-02-27 18:01:29 +0300

    #_3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str: input pointer variable
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
