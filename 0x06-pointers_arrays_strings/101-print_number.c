a5cb943b239ed8981bb0a7a4fb8f33c03c62a76e
Hillary Kiprotich <hillarykim001@gmail.com>
2023-03-02 19:36:37 +0300

    #_101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ *@n: input value
+ *Return: void
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -1 * n;
+	}
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar('0' + n % 10);
+}
