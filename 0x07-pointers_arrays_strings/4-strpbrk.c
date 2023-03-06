f55864e5ab1bd72358061e027318a3443513bc10
Hillary Kiprotich <hillarykim001@gmail.com>
2023-03-06 19:20:37 +0300

    #_4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *_strpbrk - searches a string for any of a set of bytes.
+ *@s: string to be searched
+ *@accept: character to be compared
+ *Return: char type
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int slen = 0, alen;
+
+	while (s[slen])
+	{
+		alen = 0;
+
+		while (accept[alen])
+		{
+			if (accept[alen] == s[slen])
+			{
+				s += alen;
+				return (s);
+			}
+			alen++;
+		}
+		slen++;
+	}
+
+	return ('\0');
+}
