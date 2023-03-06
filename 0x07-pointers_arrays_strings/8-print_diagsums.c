bbe4ef4528df195eaea36a9a91ef23948ed97e61
Hillary Kiprotich <hillarykim001@gmail.com>
2023-03-06 20:33:56 +0300

    #_8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,11 +9,11 @@
 
 void print_diagsums(int *a, int size)
 {
-	int x, y, sum1 = 0, sum2 = 0;
+	int x, sum1 = 0, sum2 = 0;
 
 	for (x = 0; x < size; x++)
 	{
-		sum1 += a[x * (size + 1)];
+		sum1 += a[(size + 1) * x];
 		sum2 += a[(size - 1) * (x + 1)];
 	}
 	printf("%d, %d\n", sum1, sum2);
