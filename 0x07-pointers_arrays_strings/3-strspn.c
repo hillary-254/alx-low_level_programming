99a92c367eb515eac350f85e91eb23c15794da9e
Hillary Kiprotich <hillarykim001@gmail.com>
2023-03-06 18:39:40 +0300

    #_3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strspn - compares a string with a set characters and gives length
+ *@s: string to be compared
+ *@accept: set of chars that are compared to see if it matches then gives len
+ *Return: int type
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int slen, alen = 0, len = 0;
+
+	while (accept[alen])
+	{
+		slen = 0;
+
+		while (s[slen])
+		{
+			if (accept[alen] == s[slen])
+			{
+				len++;
+			}
+			slen++;
+		}
+
+		alen++;
+	}
+
+	return (len);
+}
