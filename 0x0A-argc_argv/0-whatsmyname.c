6edc545d12ef5265f00dee519f412223b48bc418
Hillary Kiprotich <hillarykim001@gmail.com>
2023-03-10 18:36:54 +0300

    #_0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - prints the program name
+ *@argc: the size of the command line argument
+ *@argv: the pointer array of characters
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
