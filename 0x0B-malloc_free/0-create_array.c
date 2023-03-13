0b790d4ad69fb502387754f452f9923ecaac1162
Hillary Kiprotich <hillarykim001@gmail.com>
2023-03-13 16:50:39 +0300

    #_0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,10 +9,16 @@
 
 char *create_array(unsigned int size, char c)
 {
-	int x;
+	unsigned int x;
 	char *arr;
 
-	arr = (char *) malloc(size * sizeof(char));
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	arr = malloc(size * sizeof(char));
+
 	if (arr == NULL)
 	{
 		return (NULL);
