ffbb60e4a288f6d9dae5dddf2449d151d34016eb
Hillary Kiprotich <hillarykim001@gmail.com>
2023-03-23 17:01:13 +0300

    #_0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,14 +8,14 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	int sum = 0;
+	unsigned int x;
+	va_list args;
+
 	if (n == 0)
 	{
 		return (0);
 	}
-
-	int sum = 0;
-	unsigned int x;
-	va_list args;
 
 	va_start(args, n);
 
