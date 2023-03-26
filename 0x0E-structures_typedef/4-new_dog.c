1007dc2c8c45e74c6b1a881238e6201c1c9f9654
Hillary Kiprotich <hillarykim001@gmail.com>
2023-03-26 15:09:45 +0300

    #_4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,33 @@
+#include "dog.h"
+#include <string.h>
+
+/**
+ * new_dog - ...
+ *@name: ...
+ *@age: ...
+ *@owner: ...
+ *Return: ...
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *myDog;
+
+	myDog = malloc(sizeof(dog_t));
+	if (myDog == NULL)
+		return (NULL);
+
+	myDog->name = malloc(strlen(name) + 1);
+	myDog->owner = malloc(strlen(owner) + 1);
+	if (myDog->name == NULL || myDog->owner == NULL)
+	{
+		free(myDog->name);
+		free(myDog->owner);
+		free(myDog);
+		return (NULL);
+	}
+	strcpy(myDog->name, name);
+	myDog->age = age;
+	strcpy(myDog->owner, owner);
+	return (myDog);
+}
