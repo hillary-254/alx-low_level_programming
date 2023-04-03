3350ff3aa0c1ad115dd7a249edafb38476540691
Hillary Kiprotich <hillarykim001@gmail.com>
2023-04-03 21:51:03 +0300

    #_4. Free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint - frees a listint_t list.
+ * @head: pointer to the first node
+ * Return: none
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *node_n, *curr_node;
+
+	curr_node = head;
+	while (curr_node->next != NULL)
+	{
+		node_n = curr_node->next;
+		free(curr_node);
+		curr_node = node_n;
+	}
+}
