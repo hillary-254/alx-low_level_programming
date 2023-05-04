fa0396b8328718a642c564a95acc311fee2a16e5
Hillary Kiprotich <hillarykim001@gmail.com>
2023-05-04 17:14:55 +0300

    corrected betty error

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,9 +11,10 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
+
 	else
 	{
-		*n &= (~(1UL << index));
+		*n &= (~(1 << index));
 		return (1);
 	}
 }
