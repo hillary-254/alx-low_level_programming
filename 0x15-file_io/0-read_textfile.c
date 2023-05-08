22d6a720619a7f5f0aba20b3ed405058198831d2
Hillary Kiprotich <hillarykim001@gmail.com>
2023-05-08 15:55:53 +0300

    fixed betty style error

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,7 +11,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, ch_read;
 	char *memory_space;
-	
+
 	memory_space = malloc(sizeof(char *) * letters);
 	if (!filename)
 		return (0);
