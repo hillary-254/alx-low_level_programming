eb20d9e585672713aa306f01877f98af2924d870
Hillary Kiprotich <hillarykim001@gmail.com>
2023-05-08 19:18:06 +0300

    corrections

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,44 +10,54 @@
 int main(int argc, char *argv[])
 {
 	int x, f_src, f_dest;
+	char *file_from = argv[1];
+	char *file_to = argv[2];
 	int fd_from = open(argv[1], O_RDONLY);
-	int fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	char buffer[BUFFER_SIZE];
+	int fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0666);
+	char bufr[BUFFER_SIZE];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	if (fd_from < 0)
+	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
-
-	while ((x = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	if (fd_to == -1)
 	{
-		if (fd_to < 0 || write(fd_to, buffer, x) != x)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_to);
+		exit(99);
+	}
+
+	while ((x = read(fd_from, bufr, BUFFER_SIZE)) > 0)
+	{
+		if (write(fd_to, bufr, x) != x)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 			close(fd_from);
 			exit(99);
 		}
 	}
-	if (x < 0)
+	if (x == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
 	f_src = close(fd_from);
 	f_dest = close(fd_to);
-	if (f_src < 0 || f_dest < 0)
+	if (f_src == -1)
 	{
-		if (f_src < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		if (f_dest < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+
+	if (f_dest == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
 	return (0);
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,11 +1,14 @@
 #ifndef MAINH
 #define MAINH
 
+#define BUFFER_SIZE 1024
+
 #include <stdlib.h>
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <stdio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
