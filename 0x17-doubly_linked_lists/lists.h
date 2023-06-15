6c96279ea415e9fff4f762706cb94d45c844b2e3
Hillary Kiprotich <hillarykim001@gmail.com>
2023-06-15 19:03:59 +0300

    added prototypes that had earlier missed

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LISTS.H
-#define LISTS.H
+#ifndef _LISTS_H_
+#define _LISTS_H_
 
 /*Header files*/
 #include <stdlib.h>
@@ -23,4 +23,7 @@ typedef struct dlistint_s
 
 /*Function prototypes*/
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 #endif
